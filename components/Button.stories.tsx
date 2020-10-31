6b45668d970cc54daab52763a022500f55628192
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-10-31 21:43:52 +0100

    fixed storybook

--- components/Button.stories.tsx
+++ components/Button.stories.tsx
@@ -4,7 +4,3 @@ import Button from "./Button";
 storiesOf("Button", module).add("with text", () => {
   return <Button text="Hello World" />;
 });
-
-storiesOf("Button", module).add("with emoji", () => {
-  return <Button text="😀 😎 👍 💯" />;
-});
